44291d2bc14a38c65c130d8c3d2e685bda169266
a.shkondin@eastwind.ru <a.shkondin@eastwind.ru>
2017-10-04 17:44:29 +0500

    docker

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+docker stop db;
+docker rm db;
+docker stop stockpredictionapplication;
+docker rm stockpredictionapplication;
 mvn clean package -DskipTests &&\
-cp target/stockpredictionapplication.war src/main/resources/docker/app &&\
-chmod 765 src/main/resources/docker/app/stockpredictionapplication.war &&\
+cp target/stockpredictionapplication.jar src/main/resources/docker/app &&\
+chmod 765 src/main/resources/docker/app/stockpredictionapplication.jar &&\
 docker build src/main/resources/docker/db -t db &&\
 docker run --name db -h db -d db &&\
 docker build src/main/resources/docker/app -t stockpredictionapplication &&\
-docker run --name stockpredictionapplication -p 8080:8080 --link db:db -d stockpredictionapplication
+docker run --name stockpredictionapplication -p 80:80 -p 8080:8080 --link db:db -d stockpredictionapplication
